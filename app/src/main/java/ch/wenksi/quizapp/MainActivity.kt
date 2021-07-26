f8992c95353290b19990fb69465b9ce5d1ed7879
Silvan Wenk <silvan.wenk@stud.hslu.ch>
2021-07-26 11:37:19 +0200

    Create start screen

--- app/src/main/java/ch/wenksi/quizapp/MainActivity.kt
+++ app/src/main/java/ch/wenksi/quizapp/MainActivity.kt
@@ -2,11 +2,14 @@ package ch.wenksi.quizapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
     }
 }
